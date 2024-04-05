8a3d0dafe324968eee72b32ac8776de77c5ae6e6
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-04-05 12:29:33 -0600

    fix

--- hello_7segment/hello_7segment.c
+++ hello_7segment/hello_7segment.c
@@ -28,22 +28,22 @@
 #define FIRST_GPIO 2
 #define BUTTON_GPIO (FIRST_GPIO+7)
 
-// This array converts a number 0-9 to a bit pattern to send to the GPIOs
-int bits[10] = {
-        0x3f,  // 0
-        0x06,  // 1
-        0x5b,  // 2
-        0x4f,  // 3
-        0x66,  // 4
-        0x6d,  // 5
-        0x7d,  // 6
-        0x07,  // 7
-        0x7f,  // 8
-        0x67   // 9
-};
 
 /// \tag::hello_gpio[]
 int main() {
+    // This array converts a number 0-9 to a bit pattern to send to the GPIOs
+    int bits[10] = {
+            0x3f,  // 0
+            0x06,  // 1
+            0x5b,  // 2
+            0x4f,  // 3
+            0x66,  // 4
+            0x6d,  // 5
+            0x7d,  // 6
+            0x07,  // 7
+            0x7f,  // 8
+            0x67   // 9
+    };
     stdio_init_all();
     printf("Hello, 7segment - press button to count down!\n");
 
