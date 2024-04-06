a76758046367a7e78e62233f5245109ac75be062
Ivan Gordillo <gordillosolis@icloud.com>
2024-04-06 11:02:50 -0600

    add inlines functions to common

--- common/sdk_inlines.c
+++ common/sdk_inlines.c
@@ -6,3 +6,19 @@
 void gpio_set_dir_wrapper(unsigned gpio, bool out) {
     gpio_set_dir(gpio, out);
 }
+
+void gpio_pull_up_wrapper(unsigned gpio) {
+    gpio_pull_up(gpio);
+}
+
+bool gpio_get_wrapper(unsigned gpio) {
+    gpio_get(gpio);
+}
+
+void gpio_set_mask_wrapper(uint32_t mask) {
+    gpio_set_mask(mask);
+}
+
+void gpio_clr_mask_wrapper(uint32_t mask) {
+    gpio_clr_mask(mask);
+}
